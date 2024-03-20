efd956373811d3429096f5bedce598feecf972f7
Bris <fajardobrisley@gmail.com>
2024-03-20 13:02:11 +0100

    Main de pruebas

---
+++ Libft/main.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brfajard <brfajard@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 18:49:42 by brfajard          #+#    #+#             */
+/*   Updated: 2024/03/17 18:49:46 by brfajard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+/*
+	ftisalpha
+    printf("El resultado: %d\n", ft_isalpha(38));
+
+    ftisdigit
+    
+    int result;
+    result = ft_isdigit(50);
+    printf("El resultado: %d\n", result);
+    
+
+    ft_isalnum
+    printf("El resultado: %d\n", ft_isalnum(' '));
+
+    ft_isascii
+    printf("El resultado: %d\n", ft_isascii(128));
+
+    ft_isprint
+    printf("El resultado: %d\n", ft_isprint(198));
+
+    ft_strlen
+    char *c = "hola";
+    printf("El resultado: %zu\n", ft_strlen("holaaa1234"));
+
+    ft_memset
+    char buffer[40];
+    ft_memset(buffer, 'X', 2);
+    printf("El resultado: %s\n", buffer);
+*/    
+
+	return (0);
+}
