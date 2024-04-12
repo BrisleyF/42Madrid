4e4d1358bbceefb2c9423e670116dd12fc47dce1
Brisley Fajardo <test.brisley@gmail.com>
2024-04-12 15:58:51 +0200

    Configuración para el bonus

--- Libft/libft.h
+++ Libft/libft.h
@@ -12,9 +12,14 @@
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 int		ft_isalpha(int c);
 
@@ -84,4 +89,37 @@ void	ft_putendl_fd(char *s, int fd);
 
 void	ft_putnbr_fd(int n, int fd);
 
+t_list	*ft_lstnew(void *content);
+
 #endif
+
+// La palabra clave struct en C se utiliza para definir una estructura de
+// datos. Una estructura en C es una colección de variables de diferentes
+// tipos agrupadas bajo un mismo nombre.
+// La palabra clave struct nos indica que estamos definiendo una estructura
+// de datos en general, y la estructura de la lista se especifica mediante
+// la definición de los miembros particulares (content y next) que son
+// relevantes para representar un nodo en una lista enlazada.
+// `typedef` es una palabra clave en C que se utiliza para crear alias o
+// nombres alternativos para tipos de datos existentes. Es una forma
+// conveniente de dar nombres más cortos y significativos a tipos de datos
+// complejos o largos.
+// La palabra clave typedef se utiliza para definir un nuevo tipo de dato
+// llamado t_list, que es un alias para la estructura struct s_list. 
+// `void *content`: Este es un puntero void que puede apuntar a cualquier tipo
+// de dato, ya que void * es un puntero genérico en C. Este miembro se utiliza
+// para almacenar el contenido del nodo de la lista. Puede contener un puntero
+// a cualquier tipo de dato, y es responsabilidad del programador asegurarse
+// de que el contenido se interprete correctamente.
+// `struct s_list *next`: Este es un puntero que apunta al siguiente nodo en la
+// lista enlazada. Es de tipo struct s_list *, lo que significa que apunta a
+// otra estructura del mismo tipo (es decir, a otro nodo en la lista). Si next
+// es NULL, significa que este nodo es el último de la lista.
+//   Este tipo de estructura de datos es útil cuando necesitas almacenar una
+// secuencia de elementos y quieres poder agregar o eliminar elementos de manera
+// eficiente. La flexibilidad de la lista enlazada permite inserciones y
+// eliminaciones eficientes en cualquier punto de la lista.
+// Entonces, en términos generales, un nodo en una lista enlazada es una
+// estructura que contiene datos y una referencia al siguiente nodo en la
+// lista. La lista en sí misma está compuesta por una secuencia de estos nodos,
+// donde cada nodo está vinculado al siguiente nodo mediante los punteros next.
